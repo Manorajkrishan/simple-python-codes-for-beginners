a92567e63aa6ee8ede137e530190ea3eb1b2a6e3
Krishan <83907384+Manorajkrishan@users.noreply.github.com>
2024-01-07 22:58:38 +0530

    Add files via upload

---
+++ Question 4/dictionary.py
@@ -0,0 +1,12 @@
+#dittionaries
+thedit={'id':1,'name':"Krishan",'salary':5000.00}
+
+print(thedit)
+print(type(thedit))
+print()
+#assigning elements
+print("The id is : " ,thedit["id"])
+ #to add elements
+thedit["bonus"]=10000
+print()
+print(thedit)
